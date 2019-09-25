7a5269a3e0e86fc92961ba2d122e115bb82f0429
root <root@example.org>
2019-09-25 14:11:02 +0300

    updates for nova 2.3

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -15,16 +15,17 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../resources/views/partials' => resource_path('views/vendor/nova/partials'),
-        ], 'nova-ie11-views');
-
-        $this->publishes([
-            __DIR__.'/../dist/css' => public_path('vendor/nova'),
-        ], 'nova-ie11-styles');
+//        $this->publishes([
+//            __DIR__.'/../resources/views/partials' => resource_path('views/vendor/nova/partials'),
+//        ], 'nova-ie11-views');
+//
+//        $this->publishes([
+//            __DIR__.'/../dist/css' => public_path('vendor/nova'),
+//        ], 'nova-ie11-styles');
 
         Nova::serving(function (ServingNova $event) {
             Nova::script('IE11Compatibility', __DIR__.'/../dist/js/nova.js');
+            Nova::style('IE11Compatibility', __DIR__.'/../dist/css/app.css');
         });
     }
 
